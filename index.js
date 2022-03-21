9f171e982295689f883fe599b02c37410d9b9d2c
somi-kumari <somisourabh96@gmail.com>
2022-03-21 11:32:17 +0530

    mod

--- index.js
+++ index.js
@@ -58,9 +58,173 @@ const commentSchema = new mongoose.Schema(
   }
 );
 const Comment = mongoose.model("Comment", commentSchema);
+app.post("/users",
+body("first_name")
 
-app.listen(3000, async () => {
+// ...............
+
+// body('username').isEmail(),
+body("firstName")
+  .trim()
+  .not()
+  .isEmpty()
+  .bail()
+  .withMessage("First Name cannot be empty")
+  .isLength({ min: 3 })
+  .withMessage("First Name must be at least 3 characters"),
+body("email")
+  .isEmail()
+  .custom(async (value) => {
+    const user = await User.findOne({ email: value });
+
+    if (user) {
+      throw new Error("Email is already taken");
+    }
+    return true;
+  }),
+body("age")
+  .not()
+  .isEmpty()
+  .withMessage("Age cannot be empty")
+  .isNumeric()
+  .withMessage("Age must be a number between 1 and 120")
+  .custom((val) => {
+    if (val < 1 || val > 150) {
+      throw new Error("Incorrect age provided");
+    }
+    return true;
+  }),
+
+ 
+
+
+
+
+body("lastName").custom((value) => {
+  if (value && value.length < 3) {
+    throw new Error("Last Name if provided must be at least 3 characters");
+  }
+  return true;
+}),
+async (req, res) => {
   try {
-    await connect();
-  } catch {}
+    console.log(body("firstName"));
+    const errors = validationResult(req);
+    console.log({ errors });
+    if (!errors.isEmpty()) {
+      return res.status(400).send({ errors: errors.array() });
+    }
+
+    const user = await User.create(req.body);
+
+    return res.status(201).send(user);
+  } catch (err) {
+    return res.status(500).send({ message: err.message });
+  }
+}
+);
+// ......
+"/",
+// body('username').isEmail(),
+body("likes")
+  .trim()
+  .not()
+  .isEmpty()
+  .bail()
+  .withMessage("First Name cannot be empty")
+  .isLength({ min: 3 })
+  .withMessage("First Name must be at least 3 characters"),
+body("email")
+  .isEmail()
+  .custom(async (value) => {
+    const user = await User.findOne({ email: value });
+
+    if (user) {
+      throw new Error("Email is already taken");
+    }
+    return true;
+  }),
+
+
+ 
+
+
+
+
+
+async (req, res) => {
+  try {
+    console.log(body("firstName"));
+    const errors = validationResult(req);
+    console.log({ errors });
+    if (!errors.isEmpty()) {
+      return res.status(400).send({ errors: errors.array() });
+    }
+
+    const user = await User.create(req.body);
+
+    return res.status(201).send(user);
+  } catch (err) {
+    return res.status(500).send({ message: err.message });
+  }
+}
+;
+// ......
+"/",
+// body('username').isEmail(),
+body("publication")
+  .trim()
+  .not()
+  .isEmpty()
+  .bail()
+  .withMessage("")
+  .isLength({ min: 3 })
+  .withMessage("First Name must be at least 3 characters"),
+body("email")
+  .isEmail()
+  .custom(async (value) => {
+    const user = await User.findOne({ email: value });
+
+    if (user) {
+      throw new Error("Email is already taken");
+    }
+    return true;
+  }),
+
+
+ 
+
+
+
+
+body("name").custom((value) => {
+  if (value && value.length < 3) {
+    throw new Error("Last Name if provided must be at least 3 characters");
+  }
+  return true;
+}),
+async (req, res) => {
+  try {
+    console.log(body("firstName"));
+    const errors = validationResult(req);
+    console.log({ errors });
+    if (!errors.isEmpty()) {
+      return res.status(400).send({ errors: errors.array() });
+    }
+
+    const user = await User.create(req.body);
+
+    return res.status(201).send(user);
+  } catch (err) {
+    return res.status(500).send({ message: err.message });
+  }
+}
+;
+
+
+
+
+
+app.listen(3000, async (req,res) => {
+  console.log("listening on port 3000")
 });
